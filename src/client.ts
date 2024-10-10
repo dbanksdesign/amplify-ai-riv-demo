097367b0dc2bc59f1359b7bd0ac4ce14e58e82a3
Danny Banks <djb@amazon.com>
2024-10-09 22:11:09 -0700

    updates

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,4 @@
 import { generateClient } from "aws-amplify/api";
 import { Schema } from "../amplify/data/resource";
-import { createAIHooks } from "@aws-amplify/ui-react-ai";
 
 export const client = generateClient<Schema>({ authMode: "userPool" });
-export const { useAIGeneration, useAIConversation } = createAIHooks(client);
