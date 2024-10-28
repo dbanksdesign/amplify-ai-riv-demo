a83e715b0dd7db1cd78e19cce97e0ba358e1b841
Danny Banks <djb@amazon.com>
2024-10-28 13:20:38 -0700

    demo

--- src/app/listings/[id]/ReviewSummarization.tsx
+++ src/app/listings/[id]/ReviewSummarization.tsx
@@ -1,18 +1,44 @@
-import { Button } from "@aws-amplify/ui-react";
+import * as React from "react";
+import { Button, Message } from "@aws-amplify/ui-react";
 import { Listing } from "./page";
+import Markdown from "react-markdown";
+import { useAIGeneration } from "@/client";
+
+interface SummaryData {
+  summary?: string;
+}
 
 export const ReviewSummarization = ({
   reviews,
 }: {
   reviews: Listing["reviews"];
 }) => {
-  const generateSummary = async () => {};
+  const [data, setData] = React.useState<SummaryData>({});
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const generateSummary = async () => {
+    setData({ summary: "Testing" });
+  };
 
   return (
     <>
-      <Button loadingText="Generating..." onClick={generateSummary}>
-        Generate Summary
-      </Button>
+      {data?.summary ? (
+        <Message
+          variation="outlined"
+          colorTheme="info"
+          heading="AI-generated summary"
+        >
+          <Markdown>{data.summary}</Markdown>
+        </Message>
+      ) : (
+        <Button
+          isLoading={isLoading}
+          loadingText="Generating..."
+          onClick={generateSummary}
+        >
+          Generate Summary
+        </Button>
+      )}
     </>
   );
 };
