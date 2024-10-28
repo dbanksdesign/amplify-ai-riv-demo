504df4589a95be9f271acf1eb11159a8b01a984a
Danny Banks <djb@amazon.com>
2024-10-27 22:56:29 -0700

    stuff

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { getWeather } from "../functions/getWeather/resource";
-import { generateImage } from "../functions/generateImage/resource";
 
 const schema = a.schema({
   // Data Models
@@ -77,43 +76,6 @@ const schema = a.schema({
     )
     .authorization((allow) => allow.authenticated())
     .handler(a.handler.function(getWeather)),
-  generateImage: a
-    .query()
-    .arguments({
-      prompt: a.string(),
-    })
-    .returns(a.string().array())
-    .handler(a.handler.function(generateImage))
-    .authorization((allow) => [allow.authenticated()]),
-
-  // Generation routes
-  generateListing: a
-    .generation({
-      aiModel: a.ai.model("Claude 3.5 Sonnet"),
-      systemPrompt: `You are a helpful assistant that generates rental listing information based on a description. Please fill out the information as best as you can. Using the description provided create a catchy title, a bulleted list for amenities, and a longform description between 20 and 200 words. Make up information if none is provided.`,
-      inferenceConfiguration: {
-        temperature: 0.7,
-        topP: 1,
-        maxTokens: 4000,
-      },
-    })
-    .arguments({
-      description: a.string(),
-    })
-    .returns(
-      a.customType({
-        title: a.string(),
-        description: a.string(),
-        amenities: a.string(),
-        numBedrooms: a.integer(),
-        numBathrooms: a.integer(),
-        type: a.string(),
-        sleeps: a.integer(),
-        squareFeet: a.integer(),
-        price: a.integer(),
-      })
-    )
-    .authorization((allow) => [allow.authenticated()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
